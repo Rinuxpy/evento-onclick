63a05bcdb6e98e875e40870b33b85a5c444f384b
Rinuxpy <117863223+Rinuxpy@users.noreply.github.com>
2022-12-08 22:49:20 -0300

    tercero

---
+++ on.js
@@ -0,0 +1,19 @@
+function hide(element) {
+    element.remove();
+    } 
+
+c = 0
+function adicionarLike() {
+  c += 1;
+  botao.innerHTML = `${c} likes`;  
+} 
+
+c = 0
+function adicionarLike2() {
+  c += 1;
+  bota.innerHTML = `${c} likes`;  
+} 
+function login(element) {
+  element.innerText = "Logout"
+    
+}
